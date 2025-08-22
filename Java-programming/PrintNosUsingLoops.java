570dd495d58e04a0cb90e5b23aa6cd3c7409d931
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-08-22 16:40:10 +0530

    Print 1 to 100 using loops

---
+++ Java-programming/PrintNosUsingLoops.java
@@ -0,0 +1,16 @@
+
+public class PrintNosUsingLoops {
+  int i ;
+    void cal(){
+        for(i = 1 ; i<=100 ; i++)
+          {
+            System.out.println(i );
+          }
+        
+    }
+
+    public static void main(String arg[]){
+        PrintNosUsingLoops ob = new PrintNosUsingLoops();
+        ob.cal();
+    }
+}
