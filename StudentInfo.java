7b6439fbe171456fa7da1a2b399df27584d76550
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-08-28 15:06:40 +0530

    Student info

---
+++ StudentInfo.java
@@ -0,0 +1,12 @@
+ public class StudentInfo {
+    public static void main(String[] args) {
+        
+        String name = "Utsav Shrivastav";
+        String enrollment  = "A010145025018"; 
+        String date = "28-08-2025";  
+
+        System.out.println("Name      : " + name);
+        System.out.println("Roll No   : " + enrollment);
+        System.out.println("Date      : " + date);
+    }
+}
