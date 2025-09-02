1e2da552b6607dc446edaf3536611c629b63582f
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-02 19:24:25 +0530

    simple addition

---
+++ Java-programming/tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+public class SimpleCalculator {
+
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        SimpleCalculator calc = new SimpleCalculator();
+        int result = calc.add(5, 10);
+        System.out.println("Result: " + result);
+    }
+}
