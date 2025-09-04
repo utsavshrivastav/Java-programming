d1d16ce46b0ac89888cd95d10909cd73d6b171a5
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-05 04:59:53 +0530

    Exs of arithematic operators

---
+++ Java-programming/Arithematic.java
@@ -0,0 +1,12 @@
+
+public class Arithematic{
+public static void main(String[] args)
+{int x= 18;
+int y = 30;
+System.out.println(x+y);
+System.out.println(x-y);
+System.out.println(x*y);
+System.out.println(x/y);
+System.out.println(x%y);
+System.out.println(++x);
+}}
