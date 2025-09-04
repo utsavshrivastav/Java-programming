a214eb5f2cb3e26a95dc9e5d79882f6ae9b8426f
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-05 05:03:56 +0530

    Assignment opeators

---
+++ Java-programming/Assignment.java
@@ -0,0 +1,12 @@
+
+public class Assignment {
+public static void main(String[] args){
+int x= 18;
+System.out.println(x+=3);
+System.out.println(x-=3);
+System.out.println(x*=3);
+System.out.println(x/=3);
+System.out.println(x%=3);
+System.out.println(x&=3);
+}
+}
