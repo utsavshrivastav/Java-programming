e38c6fd9e5aaaec86e61dc9be5ba7ca7da0b807e
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-05 04:48:14 +0530

    print namee

---
+++ Java-programming/abcd.java
@@ -0,0 +1,12 @@
+
+ public class abcd{
+public static void main(String[] args){
+    System.out.println("Name\tMail-Id\tEmployee-Code\tPhone Number");
+System.out.println("Utsav\tutsav@388\tA1231\t3456234567");
+System.out.println("viccki\tviccki@987\tA1232\t578783233");
+System.out.println("Shreya\tShrey@889\tA1233\t9483883323");
+System.out.println("Shruti\tshruti@788\tA1234\t9934939333");
+System.out.println("Rohan\tRohan@997\tA1235\t2399493293");
+}
+ }
+
