08a4a3a7c8e8d228c1f083922daa3ea15ba7f1b5
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-05 05:08:43 +0530

    Comparision OPerators

---
+++ Java-programming/Comparision.java
@@ -0,0 +1,13 @@
+
+public class Comparision{
+public static void main(String[] args){
+int x= 18;
+int y= 30;
+System.out.println(x>y);
+System.out.println(x<y);
+System.out.println(x==y);
+System.out.println(x>=y);
+System.out.println(x<=y);
+System.out.println(x!=y);
+}
+}
