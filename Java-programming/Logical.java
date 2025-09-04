0d63e452186bb2a568a4dc900aa637bc2690a6e0
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-05 05:12:54 +0530

    Logical operators

---
+++ Java-programming/Logical.java
@@ -0,0 +1,9 @@
+
+public class Logical{
+public static void main(String[] args){
+int x= 18;
+System.out.println(x<5 && x>19);
+System.out.println(x<25 || 5<x);
+System.out.println(!(x<5 && x<45));
+} 
+}
