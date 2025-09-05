59c3dcfe8367443e9f948949014edee6f628eb7c
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 00:37:18 +0530

    Any operator related question

---
+++ Java-programming/OperatorQue.java
@@ -0,0 +1,18 @@
+
+public class OperatorQue {
+
+public static void main(String[] args)
+{int x=4;
+  if (x==1){
+System.out.println("one");}
+else if (x==2){
+System.out.println("two");}
+else if (x==3){
+System.out.println("three");}
+else if (x==4){
+System.out.println("fourth");}
+else if (x==5){
+System.out.println("five");}
+}
+}
+
