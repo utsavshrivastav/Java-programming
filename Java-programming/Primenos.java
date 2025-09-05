708dba9a408266e553682b70a43d8afa703c2f72
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 01:52:44 +0530

    Prime no print

---
+++ Java-programming/Primenos.java
@@ -0,0 +1,16 @@
+public class Primenos {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            int flag = 0;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = 1;
+                    break;  // stop checking once divisor found
+                }
+            }
+            if (flag == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
