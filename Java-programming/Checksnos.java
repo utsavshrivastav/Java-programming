3de8c9c9c8c70e0b967b758f23617e3c183492f6
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 01:47:13 +0530

    +ve , -ve , and zero

---
+++ Java-programming/Checksnos.java
@@ -0,0 +1,13 @@
+    import java.util.Scanner;
+
+public class Checksnos {
+
+public static void main(String[] args)
+{ Scanner sc=new Scanner(System.in);
+System.out.print("Enter a number: ");
+  int num=sc.nextInt();
+   if (num>0){System.out.println("Positive number");}
+   else if (num==0){System.out.println("zero");}
+   else {System.out.println("Negative number");}
+     }}
+
