39a8b2587c27e7773b8f09d5ab55f9904090cfe1
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 01:26:34 +0530

    Grade system

---
+++ Java-programming/Comparison.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Comparison {
+    
+
+public static void main(String[] args)
+{ Scanner sc=new Scanner(System.in);
+System.out.print("Enter a number: ");
+  int num=sc.nextInt();
+   if (num>100 && num<0){System.out.println("Enter valid marks");}
+   else{
+        if(num>=80) { System.out.println("Grade A+");}
+        else if (num<80 && num>=70){  System.out.println("Grade A");}
+        else if(num<70 && num>=60){ System.out.println("Grade B");}
+        else if (num<60 && num >=50){ System.out.println("Grade C");}
+        else if ( num<50 && num >=40){ System.out.println("Grade D");}
+        else{ System.out.println("Fail");}}
+     }}
+
