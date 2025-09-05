2379ebd8171b7fadeff4392a855a51894283dfe4
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 01:10:05 +0530

    leapYear

---
+++ Java-programming/LeapYears.java
@@ -0,0 +1,27 @@
+
+ import java.util.Scanner;
+
+public class LeapYears {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = ip.nextInt();
+
+        if (num % 4 == 0) {
+            if (num % 100 == 0) {
+                if (num % 400 == 0) {
+                    System.out.println("Leap year");
+                } else {
+                    System.out.println("Not leap year");
+                }
+            } else {
+                System.out.println("Leap year");
+            }
+        } else {
+            System.out.println("Not leap year");
+        }
+    }
+}
+
+
