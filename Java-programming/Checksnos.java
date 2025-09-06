481ca758e47bea8a94afc51c7bd6bd87cd3678c5
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 19:53:18 +0530

    Solving Scanner class problem

--- Java-programming/Checksnos.java
+++ Java-programming/Checksnos.java
@@ -1,13 +1,31 @@
     import java.util.Scanner;
-
-public class Checksnos {
+    public class Checksnos {
 
 public static void main(String[] args)
-{ Scanner sc=new Scanner(System.in);
-System.out.print("Enter a number: ");
-  int num=sc.nextInt();
-   if (num>0){System.out.println("Positive number");}
-   else if (num==0){System.out.println("zero");}
-   else {System.out.println("Negative number");}
-     }}
+{
+   Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a number: ");
+      int num=sc.nextInt();
+
+   if (num>0){
+    System.out.println("Positive number");
+               }
+
+   else if (num==0){
+    System.out.println("zero");
+              }
+
+   else {
+    System.out.println("Negative number");
+             }
+         sc.close(); // good practice to close scanner
+
+            }
+          }   
+
+          
+        
+          
+
+     
 
