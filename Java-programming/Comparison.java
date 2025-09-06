41b177634a403b73bfefe4ebe01f927e7b92e435
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 19:48:53 +0530

    Scanner closing error

--- Java-programming/Comparison.java
+++ Java-programming/Comparison.java
@@ -1,18 +1,48 @@
 import java.util.Scanner;
-public class Comparison {
-    
+public class Comparison{
+  
 
-public static void main(String[] args)
-{ Scanner sc=new Scanner(System.in);
+public static void main(String[] args){
+
+int num ;
+
 System.out.print("Enter a number: ");
-  int num=sc.nextInt();
-   if (num>100 && num<0){System.out.println("Enter valid marks");}
-   else{
-        if(num>=80) { System.out.println("Grade A+");}
-        else if (num<80 && num>=70){  System.out.println("Grade A");}
-        else if(num<70 && num>=60){ System.out.println("Grade B");}
-        else if (num<60 && num >=50){ System.out.println("Grade C");}
-        else if ( num<50 && num >=40){ System.out.println("Grade D");}
-        else{ System.out.println("Fail");}}
-     }}
+Scanner sc = new Scanner(System.in);
+
+   num = sc.nextInt();
+
+   if (num>100 || num<0)
+         {
+    System.out.println("Enter valid marks");
+        }
+
+   else
+   {
+        if(num>=80) {
+           System.out.println("Grade A+");
+          }
+        else if (num<80 && num>=70){
+          System.out.println("Grade A");
+               }
+
+        else if(num<70 && num>=60){ 
+          System.out.println("Grade B");
+                }
+        else if (num<60 && num >=50){
+           System.out.println("Grade C");
+                  }
+        else if ( num<50 && num >=40){
+           System.out.println("Grade D");
+                  }
+        else{ 
+          System.out.println("Fail");
+               }
+              }
+        sc.close(); // good practice to close scanner
+
+
+            } 
+          }   
+     
+    
 
