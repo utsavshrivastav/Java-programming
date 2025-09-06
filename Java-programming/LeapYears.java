05bf01d3824fe5a1191796176dccd4ed4923aef1
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-06 20:08:10 +0530

    error solving

--- Java-programming/LeapYears.java
+++ Java-programming/LeapYears.java
@@ -1,27 +1,34 @@
 
  import java.util.Scanner;
-
-public class LeapYears {
+ public class LeapYears{
     public static void main(String[] args) {
-        Scanner ip = new Scanner(System.in);
+     Scanner ip = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+    System.out.print("Enter a number: ");
         int num = ip.nextInt();
 
         if (num % 4 == 0) {
             if (num % 100 == 0) {
-                if (num % 400 == 0) {
-                    System.out.println("Leap year");
-                } else {
-                    System.out.println("Not leap year");
-                }
-            } else {
-                System.out.println("Leap year");
-            }
-        } else {
+                if (num % 400 == 0)
+                 {
+         System.out.println("Leap year");
+                 } 
+
+                else 
+                {
             System.out.println("Not leap year");
-        }
-    }
-}
+                }
+        }        
 
+                      else {
+                       System.out.println("Leap year");
+                           }
+                        } 
+                              else {
+                                System.out.println("Not leap year");
+                                   }
+                                   ip.close(); 
 
+                                }}
+                                
+                            
