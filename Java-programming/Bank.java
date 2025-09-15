20efc1bb695b434bdf8151f6870cc6270d0895e5
utsavshrivastav <thevickyshrivasatav@gmail.com>
2025-09-16 01:10:08 +0530

    Package

---
+++ Java-programming/Bank.java
@@ -0,0 +1,6 @@
+public class Bank {
+    int Accno;
+    String holdno;
+    int bal;
+    
+}
---
+++ Java-programming/Packagees/MyCalculator.java
@@ -0,0 +1,16 @@
+package Packagees;
+
+ public class MyCalculator{
+        public int Add(int x,int y)
+        {
+            return x+y;
+        }
+       public int Subtract(int x,int y)
+        {
+            return x-y;
+        }
+        public int Product(int x,int y)
+        {
+            return x*y;
+        } }
+
